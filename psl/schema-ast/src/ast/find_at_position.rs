88cb30511541332935643df2ec24477e00665192
Sophie Atkins <atkins@prisma.io>
2024-06-15 03:46:52 +0200

    Apparently we had Type as a variant of FieldPosition, but it was never actually returned as a valid variant, so now it is.
    Made `iter_tops` on walkers public. Unsure if there's a better way about this; it seemed the most convenient for right now.
    
    Added a helper to FieldType to retrieve names.
    
    Further clean-up of hover

--- psl/schema-ast/src/ast/find_at_position.rs
+++ psl/schema-ast/src/ast/find_at_position.rs
@@ -162,6 +162,10 @@ impl<'ast> FieldPosition<'ast> {
             }
         }
 
+        if field.field_type.span().contains(position) {
+            return FieldPosition::Type(field.field_type.name());
+        }
+
         FieldPosition::Field
     }
 }
